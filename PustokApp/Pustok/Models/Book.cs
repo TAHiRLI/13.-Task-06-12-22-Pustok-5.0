5fa66d341af156e85793b77a6acdae895b406590
TAHiRLI <tahrli.tahirr@gmail.com>
2022-12-07 02:21:42 +0400

    task done

---
+++ PustokApp/Pustok/Models/Book.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Pustok.Models
+{
+    public class Book: BaseEntity
+    {
+        public int GenreId { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public int AuthorId { get; set; }
+        public bool StockStatus { get; set; }
+        public bool IsSpecial { get; set; }
+        public bool IsNew { get; set; }
+
+        [Column(TypeName ="money")]
+        public decimal CostPrice { get; set; }
+
+
+        [Column(TypeName = "money")]
+        public decimal SalePrice { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal DiscountPercent { get; set; }
+
+
+
+
+        public Genre Genre { get; set; }
+        public Author Author { get; set; }
+
+
+        public List<Review> Reviews { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+        public List<BookImage> BookImages { get; set; }
+    }
+}
